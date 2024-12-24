14ce28686ccf2e45f4381857d808d66d8ec30d60
syuuuuut <aaaa.kanh@gmail.com>
2024-12-24 13:07:46 +0900

    update

--- sketch.js
+++ sketch.js
@@ -1,24 +1,29 @@
 let img, sorted_img;
 let img_mask;
-
 let threshold = 130;
-
 let isVisibleMask = false;
 let isVertical = false;
-
+let isSortHighToLow = false;
+let customFont;
 
 function preload() {
     img = loadImage('./assets/face.jpg');
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight, WEBGL);
     pixelDensity(1);
+    setAttributes('willReadFrequently', true);
+    customFont = loadFont('assets/Arial.ttf');
 }
 
 function draw() {
     background(0);
 
+    // WEBGL座標系での描画位置調整
+    translate(-width / 2, -height / 2);
+
+    // 元画像の描画
     image(img, 0, 0);
 
     sorted_img = createImage(img.width, img.height);
@@ -26,179 +31,258 @@ function draw() {
     img_mask = createImage(img.width, img.height);
     img_mask.copy(img, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
 
-    // sorted_img = img_sort_pixels(sorted_img);
-    sorted_img = img_sort_pixels_with_mask(sorted_img);
+    sorted_img = sortImagePixels(sorted_img, isVertical);
 
+    // 処理後の画像の描画
     if (isVisibleMask) {
-        img_mask = img_create_mask(img_mask);
-        image(img_mask, img.width, 0);
+        image(createMask(img_mask), img.width, 0);
     } else {
         image(sorted_img, img.width, 0);
     }
+
+    // threshold値の表示位置調整
+    push();
+    textFont(customFont);
+    textSize(20);
+    fill(30);
+    text('Threshold: ' + threshold, 10, 20);
+    pop();
+}
+/*
+    * @img {p5.Image} 画像インスタンス
+    * @x {Number} 画像内のx座標
+    * @y {Number} 画像内のy座標
+    * @returns {Array} [R, G, B, A]
+    * 
+    * 画像インスタンスの指定した座標のピクセル値を取得する
+    * 例: getPixelAt(img, 0, 0);
+    * 画像インスタンスの左上のピクセルの値を取得する
+    */
+
+function getPixelAt(img, x, y) {
+    const index = (y * img.width + x) * 4;
+    return [
+        img.pixels[index],
+        img.pixels[index + 1],
+        img.pixels[index + 2],
+        img.pixels[index + 3]
+    ];
 }
 
+/*
+    * @img {p5.Image} 画像インスタンス
+    * @x {Number} 画像内のx座標
+    * @y {Number} 画像内のy座標
+    * @pixel {Array} [R, G, B, A]
+    * @returns {void}
+    * 
+    * 画像インスタンスの指定した座標にピクセルをセットする
+    * 画像インスタンスのピクセル値を更新するため、updatePixels()が必要
+    * 例: setPixelAt(img, 0, 0, [255, 0, 0, 255]);
+    * 画像インスタンスの左上のピクセルを赤色に変更する
+    */
 
-function img_sort_pixels(img) {
-    img.loadPixels();
+function setPixelAt(img, x, y, pixel) {
+    const index = (y * img.width + x) * 4;
+    img.pixels[index] = pixel[0];
+    img.pixels[index + 1] = pixel[1];
+    img.pixels[index + 2] = pixel[2];
+    img.pixels[index + 3] = pixel[3];
+}
 
-    // 横方向にソートする場合を考える
-    // 各行について for 文を回す
-    for (let y = 0; y < img.height; y++) {
+/*
+    * @pixel {Array} [R, G, B, A]
+    * @returns {Number} 0 ~ 255
+    *  
+    * 画素の輝度を取得する
+    * 例: getBrightness([255, 255, 255, 255]);
+    * 白色の画素の輝度を取得する
+    */
 
-        // 行の中のピクセルを取得して配列に格納
-        let rowPixels = [];
-        for (let x = 0; x < img.width; x++) {
-            let index = (y * img.width + x) * 4;
-            rowPixels.push([
-                img.pixels[index],
-                img.pixels[index + 1],
-                img.pixels[index + 2],
-                img.pixels[index + 3],
-            ]);
-        }
+function getBrightness(pixel) {
+    return 0.2126 * pixel[0] + 0.7152 * pixel[1] + 0.0722 * pixel[2];
+}
 
-        // 輝度基準でソート
-        // Rec.709 : Y = 0.2126 * R + 0.7152 * G + 0.0722 * B
-        rowPixels.sort((a, b) => {
-            let va = 0.2126 * a[0] + 0.7152 * a[1] + 0.0722 * a[2];
-            let vb = 0.2126 * b[0] + 0.7152 * b[1] + 0.0722 * b[2];
-            return vb - va;
-        });
+/*
+    * @img {p5.Image} 画像インスタンス
+    * @lineIndex {Number} 画像内の行番号
+    * @vertical {Boolean} 縦方向の場合はtrue、横方向の場合はfalse
+    * @returns {Array} [pixel1, pixel2, ...]
+    * 
+    * 画像インスタンスの指定した行のピクセル値を取得する
+    * 例: getLine(img, 0, false);
+    * 返り値: 画像インスタンスの上端の行のピクセル値の配列
+    */
 
-        // 輝度順で並び替えたピクセルで再度画像を構築
-        for (let x = 0; x < img.width; x++) {
-            let index = (y * img.width + x) * 4;
-            img.pixels[index] = rowPixels[x][0];
-            img.pixels[index + 1] = rowPixels[x][1];
-            img.pixels[index + 2] = rowPixels[x][2];
-            img.pixels[index + 3] = rowPixels[x][3];
-        }
+function getLine(img, lineIndex, vertical) {
+    const linePixels = [];
+    const length = vertical ? img.height : img.width;
+
+    for (let i = 0; i < length; i++) {
+        const [x, y] = vertical ? [lineIndex, i] : [i, lineIndex];
+        linePixels.push(getPixelAt(img, x, y));
     }
-    img.updatePixels();
-    return img;
+    return linePixels;
 }
 
-function img_sort_pixels_with_mask(img) {
-    img.loadPixels();
+/*
+    * @img {p5.Image} 画像インスタンス
+    * @lineIndex {Number} 画像内の行番号
+    * @pixels {Array} [pixel1, pixel2, ...]
+    * @vertical {Boolean} 縦方向の場合はtrue、横方向の場合はfalse
+    * @returns {void}
+    * 
+    * 画像インスタンスの指定した行にピクセルをセットする
+    * 画像インスタンスのピクセル値を更新するため、updatePixels()が必要
+    * 例: setLine(img, 0, [pixel1, pixel2, ...], false);
+    */
 
-    for (let y = 0; y < img.height; y++) {
-        // まず行のマスクを作成
-        let maskRow = [];
-        let pixelGroups = [];
-        let currentGroup = [];
-        let currentMask = -1;
+function setLine(img, lineIndex, pixels, vertical) {
+    pixels.forEach((pixel, i) => {
+        const [x, y] = vertical ? [lineIndex, i] : [i, lineIndex];
+        setPixelAt(img, x, y, pixel);
+    });
+}
 
-        // マスクを作成しながらグループを特定
-        for (let x = 0; x < img.width; x++) {
-            let index = (y * img.width + x) * 4;
-            let brightness = get_brightness(
-                img.pixels[index],
-                img.pixels[index + 1],
-                img.pixels[index + 2]
-            );
-            let maskValue = brightness > threshold ? 255 : 0;
+/*
+    * @pixels {Array} [pixel1, pixel2, ...]
+    * @returns {Array} [{mask: 0 or 255, pixels: [pixel1, pixel2, ...], startIndex: 0}, ...]
+    * 
+    * 画素のグループを作成する
+    * 例: createPixelGroups([pixel1, pixel2, ...]);
+    * 返り値: [{mask: 0 or 255, pixels: [pixel1, pixel2, ...], startIndex: 0}, ...]
+    * 画素のグループの配列
+        * mask: 0 or 255
+        * pixels: 画素の配列
+        * startIndex: 画素の配列の先頭のインデックス
+    */
 
-            // 新しいグループの開始
-            if (maskValue !== currentMask) {
-                if (currentGroup.length > 0) {
-                    pixelGroups.push({
-                        mask: currentMask,
-                        pixels: currentGroup,
-                        startX: x - currentGroup.length
-                    });
-                }
-                currentGroup = [];
-                currentMask = maskValue;
-            }
+function createPixelGroups(pixels) {
+    const groups = [];
+    let currentGroup = [];
+    let currentMask = -1;
 
-            currentGroup.push([
-                img.pixels[index],
-                img.pixels[index + 1],
-                img.pixels[index + 2],
-                img.pixels[index + 3],
-            ]);
-        }
-        // 最後のグループを追加
-        if (currentGroup.length > 0) {
-            pixelGroups.push({
-                mask: currentMask,
-                pixels: currentGroup,
-                startX: img.width - currentGroup.length
-            });
-        }
+    pixels.forEach((pixel, index) => {
+        const maskValue = getBrightness(pixel) > threshold ? 255 : 0;
 
-        // 明るいグループ（mask=255）のみをソート
-        pixelGroups.forEach(group => {
-            if (group.mask === 255) {
-                group.pixels.sort((a, b) => {
-                    let va = get_brightness(a[0], a[1], a[2]);
-                    let vb = get_brightness(b[0], b[1], b[2]);
-                    return vb - va;
+        if (maskValue !== currentMask) {
+            if (currentGroup.length > 0) {
+                groups.push({
+                    mask: currentMask,
+                    pixels: currentGroup,
+                    startIndex: index - currentGroup.length
                 });
             }
-        });
+            currentGroup = [];
+            currentMask = maskValue;
+        }
+        currentGroup.push(pixel);
+    });
 
-        // ソート済みのピクセルを元の画像に書き戻す
-        pixelGroups.forEach(group => {
-            group.pixels.forEach((pixel, i) => {
-                let x = group.startX + i;
-                let index = (y * img.width + x) * 4;
-                img.pixels[index] = pixel[0];
-                img.pixels[index + 1] = pixel[1];
-                img.pixels[index + 2] = pixel[2];
-                img.pixels[index + 3] = pixel[3];
-            });
+    if (currentGroup.length > 0) {
+        groups.push({
+            mask: currentMask,
+            pixels: currentGroup,
+            startIndex: pixels.length - currentGroup.length
         });
     }
+
+    return groups;
+}
+
+/*
+    * @img {p5.Image} 画像インスタンス
+    * @vertical {Boolean} 縦方向の場合はtrue、横方向の場合はfalse
+    * @returns {p5.Image} 画像インスタンス
+    * 
+    * 画像の画素をソートする
+    * 例: sortImagePixels(img, true);
+    * 返り値: 縦方向に輝度の高い順にソートされた画像インスタンス
+    */
+
+function sortImagePixels(img, vertical) {
+    img.loadPixels();
+    const length = vertical ? img.width : img.height;
+
+    for (let i = 0; i < length; i++) {
+        const linePixels = getLine(img, i, vertical);
+        const groups = createPixelGroups(linePixels);
+
+        groups.forEach(group => {
+            if (group.mask === 255) {
+                if (isSortHighToLow) {
+                    group.pixels.sort((a, b) => getBrightness(b) - getBrightness(a));
+                }
+                else {
+                    group.pixels.sort((a, b) => getBrightness(a) - getBrightness(b));
+                }
+            }
+        });
+
+        const sortedPixels = [];
+        groups.forEach(group => {
+            sortedPixels.push(...group.pixels);
+        });
+
+        setLine(img, i, sortedPixels, vertical);
+    }
+
     img.updatePixels();
     return img;
 }
 
-function get_brightness(r, g, b) {
-    return 0.2126 * r + 0.7152 * g + 0.0722 * b;
-}
+/*
+    * @img {p5.Image} 画像インスタンス
+    * @returns {p5.Image} 画像インスタンス
+    *
+    * 画像のマスクを作成する
+    * 例: createMask(img);
+    * 返り値: 画像インスタンスのマスク
+    * 画像インスタンスの輝度がthresholdより高い場合は白、それ以外は黒
+    * 画像インスタンスのピクセル値を更新するため、updatePixels()が必要
+    * 画像インスタンスのマスクを表示する場合はisVisibleMaskをtrueにする
+    */
 
-function img_create_mask(img) {
+function createMask(img) {
     img.loadPixels();
 
     for (let y = 0; y < img.height; y++) {
-        let rowPixels = [];
         for (let x = 0; x < img.width; x++) {
-            let index = (y * img.width + x) * 4;
-            rowPixels.push([
-                img.pixels[index],
-                img.pixels[index + 1],
-                img.pixels[index + 2],
-                img.pixels[index + 3],
-            ]);
-        }
-
-        for (let x = 0; x < img.width; x++) {
-            let index = (y * img.width + x) * 4;
-            let va = 0.2126 * rowPixels[x][0] + 0.7152 * rowPixels[x][1] + 0.0722 * rowPixels[x][2];
-            if (va <= threshold) {
-                img.pixels[index] = 0;
-                img.pixels[index + 1] = 0;
-                img.pixels[index + 2] = 0;
-                img.pixels[index + 3] = 255;
-            } else {
-                img.pixels[index] = 255;
-                img.pixels[index + 1] = 255;
-                img.pixels[index + 2] = 255;
-                img.pixels[index + 3] = 255;
-            }
+            const pixel = getPixelAt(img, x, y);
+            const brightness = getBrightness(pixel);
+            const maskValue = brightness > threshold ? 255 : 0;
+            setPixelAt(img, x, y, [maskValue, maskValue, maskValue, 255]);
         }
     }
+
     img.updatePixels();
     return img;
 }
 
-function toggleMask() {
-    isVisibleMask = !isVisibleMask;
-}
 
 function keyPressed() {
-    if (key == 'm' | key == 'M') {
+    function toggleMask() {
+        isVisibleMask = !isVisibleMask;
+    }
+    function toggleDirection() {
+        isVertical = !isVertical;
+    }
+    function toggleSortOrder() {
+        isSortHighToLow = !isSortHighToLow;
+    }
+    if (key === 'm' || key === 'M') {
         toggleMask();
+    }
+    if (key === 'v' || key === 'V') {
+        toggleDirection();
+    }
+    if (key === 's' || key === 'S') {
+        toggleSortOrder();
+    }
+    if (keyCode === UP_ARROW) {
+        threshold = constrain(threshold + 5, 0, 255);
+    }
+    if (keyCode === DOWN_ARROW) {
+        threshold = constrain(threshold - 5, 0, 255);
     }
 }
